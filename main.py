d68e68a23456cd13d697ed1a290f872db8b0bd9c
Elina Norenberga <elina.norenberga@edu.rtu.lv>
2023-03-16 18:42:05 +0200

    uzdevums

--- main.py
+++ main.py
@@ -1,31 +1,82 @@
 # python3
 
+class Process:
+    def __init__(self, number, processing, seconds):
+        self.number = number
+        self.processing = processing
+        self.seconds = seconds
+
+
+class Data:
+    def __init__(self, number, taken):
+        self.number = number
+        self.taken = taken
+
+
 def parallel_processing(n, m, data):
     output = []
-    # TODO: write the function for simulating parallel tasks, 
-    # create the output pairs
+    processes = []
+    for process in range(n):
+        processes.append(Process(process, False, 0))
+
+    dataList = []
+    for da in data:
+        dataList.append(Data(da, False))
+
+    seconds = 0
+
+    for da in dataList:
+        taken = False
+        while taken == False:
+            for process in processes:
+                if (process.seconds <= seconds):
+                    process.processing = False
+                if taken == False:
+                    taken = add_job(process, seconds, da, output)
+            if (taken == False):
+                seconds = seconds + 1
 
     return output
 
+
+def add_job(process, seconds, da, output):
+    if (process.processing == False and process.seconds <= seconds):
+        process.processing = True
+        process.seconds = process.seconds + da.number
+        output.append([process.number, seconds])
+        return True
+    else:
+        return False
+
+
 def main():
-    # TODO: create input from keyboard
-    # input consists of two lines
-    # first line - n and m
-    # n - thread count 
-    # m - job count
     n = 0
     m = 0
 
-    # second line - data 
-    # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
     data = []
 
-    # TODO: create the function
+    ievade = input()
+    text = ""
+    if ievade == "F":
+        text = input()
+        with open(text) as f:
+            lines = f.readlines()
+            array = lines[0].split()
+            n = int(array[0])
+            m = int(array[1])
+            array = lines[1].split()
+            for a in array:
+                data.append(int(a))
+    if ievade == "I":
+        n = int(input("Enter number of elements : ")) 
+        for i in range(0, n):
+            ele = int(input())
+            data.append(ele)
+
     result = parallel_processing(n,m,data)
     
-    # TODO: print out the results, each pair in it's own line
-
-
+    for i, j in result:
+        print(i, j)
 
 if __name__ == "__main__":
     main()
